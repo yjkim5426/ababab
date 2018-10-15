844cbf758ff41bd9db0b760c34884f3e1dad8ad6
YoungJin Kim <yjkim5426@gmail.com>
2018-10-15 23:41:11 +1300

    Update abab2.js

--- abab2.js
+++ abab2.js
@@ -1,42 +1,49 @@
+//var originalArray = ["a", "b", "a", "b"]; //1
 //var originalArray = ["a", "b", "a", "b", "a", "b"]; //2
- var originalArray = ["a", "b", "a", "b", "a", "b", "a", "b"]; //3
-
+//var originalArray = ["a", "b", "a", "b", "a", "b", "a", "b"]; //3
+//var originalArray = ["a", "a", "a", "a", "a"]; //4 - Exception
+// var originalArray = ["a", "b", "c", "a", "b","c"]; //1
+var originalArray = ["a", "b", "c", "d", "a", "b", "c"]; //0
 
 function abab(array) {
-    var count = 0;
-    //전부 같은 문자로만 구성되었는지 확인 (예외처리)
-    if(checkSameException(array)){
-        count = array.length -1;
+  var count = 0;
+  //전부 같은 문자로만 구성되었는지 확인 (예외처리)
+  if (checkSameException(array)) {
+    count = array.length - 1;
+    return count;
+  } 
+  else {
+    for (var i = 2; i <= array.length / 2; i++) {
+      if (patternCheck(i, array)) {
+        count = array.length / i - 1;
+        return count;
+      }
     }
-    else{
-        for(i=0; i<array.length/2; i++){
-            if(patternCheck(i, array)){
-                count = (array.length/i) -1;
-            }
-        }
-    }
-    console.log('Answer: ' + count);
+    return count;
+  }
 }
 
-function patternCheck(patternLength, checkArray){
-    for( i=0, j=0; i<checkArray.length; i++, j++){
-        if( checkArray[i] != checkArray[j]){
-            return false;
-        }
-        if( j == patternLength) {
-            j = 0;
-        }
+function patternCheck(patternLength, checkArray) {
+  for (var i = 0, j = 0; i < checkArray.length; i++) {
+    if (checkArray[i] != checkArray[j]) {
+      return false;
     }
-    return true;
+    if (j == patternLength - 1) {
+      j = 0;
+    } else {
+      j++;
+    }
+  }
+  return true;
 }
 
-function checkSameException(checkArray){
-    for(var i=1; i<checkArray.length; i++){
-        if(checkArray[i] != checkArray[0]){
-            return false;
-        }
+function checkSameException(checkArray) {
+  for (var i = 1; i < checkArray.length; i++) {
+    if (checkArray[i] != checkArray[0]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
-abab(originalArray);
+console.log("answer: " + abab(originalArray));
